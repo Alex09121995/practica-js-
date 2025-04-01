f51c7dc1478a5eb89a94a3f61771c05db5c58b54
alex <alexaghm411@gmail.com>
2025-04-01 13:29:15 -0400

    mi primer repositorio

---
+++ ejecicio-1.js
@@ -0,0 +1,46 @@
+const notas = [98,87,99,100]
+// metodos para agregar o eliminar elementos de un array 
+notas.push(63); // agrega al final del array 
+notas.pop();// elemina el ultimo 
+notas.unshift(91);// agrega al inicio 
+notas.shift();// elimina el primero
+
+
+for ( let i=0 ; i < notas.length; i++ ){
+     console.log(notas[i])
+
+}
+
+// Metodo para buscar elementos 
+const comidas = ["locrio","sancocho","asopao","mofongo"]
+console.log(comidas.indexOf("asopao"));// Busca la posicion de un elemento
+console.log(comidas.includes("mofongo"));// Verifica si un elemento existe 
+
+// Metodos para modificar un array
+const colores = ["azul","roja","amarrillo","verde"]
+
+colores.splice(1,0)// Elimina elementos especifico por su posicion
+colores.splice(1,2, "violeta")// agrega elementos por su posicion
+console.log(colores)
+
+//metodo para recorrer un array 
+
+// Ejecuta una funcion por cadaa elemento
+comidas.forEach((comida) => {
+     console.log(`plato: ${comida}`);
+});
+// Crea un array nuevo trasformando los elementos
+let nuevasNotas = notas.map(nota=> nota + 2);
+console.log(nuevasNotas);
+
+// Metodos para ordenar o filtrar
+ let numeros = [16,11,24,32,12]
+// Ordena los elementos de menor a mayor
+numeros.sort();
+console.log(numeros)
+// Ordena los elementos de mayor a menor
+numeros.reverse();
+console.log(numeros);
+// Filtra elementos segun la condiciones que le apliquemos en la funcion
+let mayorDeEdad = numeros.filter(numero=> numero >= 18  )
+console.log(mayorDeEdad)
